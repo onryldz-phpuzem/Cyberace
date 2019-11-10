3080b01a845b36dfb7d980a5d891b03a86ae69ca
onryldz-phpuzem <onuryildiz038@gmail.com>
2019-11-10 16:28:32 +0300

    Master

---
+++ Assets/Scripts/PlatformCreator.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class PlatformCreator : MonoBehaviour
+{
+    [Header("[Prefabs]")]
+    public GameObject regularPlaform;
+    public GameObject[] specialPlatforms;
+
+    [Header("[Settings]")]
+    [Range(0,100)]
+    public int specialPlatformPercentage;
+    void Start()
+    {
+
+        for(int i=0;i<4;i++)
+        {
+            int randomPercentage = Random.Range(0, 100);
+            int randomSpecialPlatformIndex = Random.Range(0, specialPlatforms.Length);
+
+            GameObject objectToBeCreated;
+            objectToBeCreated = randomPercentage < specialPlatformPercentage ? specialPlatforms[randomSpecialPlatformIndex] : regularPlaform;
+
+            GameObject platform = Instantiate(objectToBeCreated, transform.GetChild(i).position, transform.GetChild(i).rotation);
+            platform.transform.SetParent(transform.GetChild(i));
+            
+        }
+        Debug.Log(transform.lossyScale.normalized.z);
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    // Use this for initialization
+    public float rotateSpeed;
+    public bool x;
+    public bool y;
+    public bool z;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x)
+        {
+            transform.Rotate(rotateSpeed, 0, 0);
+        }
+        if (y)
+        {
+            transform.Rotate(0, rotateSpeed, 0);
+        }
+        if (z)
+        {
+            transform.Rotate(0, 0, rotateSpeed);
+        }
+    }
+}
